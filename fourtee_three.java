1b3f40da44bca0110df0b8c6f502f13a9bc9c895
Ayush Pratap Singh <ayushpratapno1gmail.com>
2025-09-18 21:03:21 +0530

    update fourtee_three.java

--- fourtee_three.java
+++ fourtee_three.java
@@ -1,12 +1,15 @@
 public class fourtee_three {
-    // Creating a Function named helloworld
-    public static void helloworld(){
-        System.out.println("Hello world!");
-        String str = "Hey bro";
-        System.out.println(str+" how are you");
-    }
+    // INVERTED & ROTATED HALF-PYRAMID pattern (ADVANCED PATTERN-1)
     public static void main(String args[]){
-        helloworld();
-        
+        int num = 4;
+        for (int i = 1; i<=num; i++){
+            for (int j = 1; j<=num-i; j++){
+                System.out.print(" ");
+            }
+            for (int k = 1; k<=i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
