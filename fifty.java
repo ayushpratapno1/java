8e39632f19d8de032df68396c5a3f9231fa01d83
Ayush Pratap Singh <ayushpratapno1gmail.com>
2025-10-01 17:29:41 +0530

    Fifty java file

---
+++ fifty.java
@@ -0,0 +1,9 @@
+public class fifty {
+    // Function example
+    public static void helloworld() {
+        System.out.println("Hello world!");
+    }
+    public static void main(String[] args) {
+        helloworld();
+    }
+}
